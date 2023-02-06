5c61d6a583b302170a693ce02c2f29d354cedd52
Galen Sho Sato <galen.sato@gmail.com>
2023-02-06 15:12:44 -0500

    Completed all deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +7,69 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  
+
+  
+  useEffect(()=> {
+      fetch('http://localhost:3001/pets')
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+
+  }, [])
+
+
+  function onChangeType() {
+    let typeSet = document.getElementById('type')
+    return setFilters({
+      type: typeSet.value
+    })
+
+  }
+  
+  function onFindPetsClick () {
+    let x = filters.type
+
+    if (x === 'all') {
+      fetch('http://localhost:3001/pets')
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+
+    }
+
+    if (x === 'cat') {
+      fetch('http://localhost:3001/pets?type=cat')
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+      
+    }
+
+    if(x === 'dog') {
+      fetch('http://localhost:3001/pets?type=dog')
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+      
+    }
+
+    if(x === 'micropig') {
+      fetch('http://localhost:3001/pets?type=micropig')
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+      
+  } 
+  }
+
+  function onAdoptPet(id) {
+    const newPets = pets.map(pet => {
+      
+      if (pet.id === id) {
+        pet.isAdopted = 'true'
+        return pet
+        
+      }
+    return pet
+    })
+    setPets(newPets)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +79,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
@@ -26,4 +90,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
